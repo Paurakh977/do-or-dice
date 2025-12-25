ef35c4d7eb9b9d7e46f1951c5e28f7100f0beb07
Paurakh Raj Pandey <124509859+Paurakh977@users.noreply.github.com>
2025-12-26 00:26:08 +0545

    simulating turn by turn sleep basde dice rolls

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from __future__  import annotations
 from src import Randomizer
 from src import Player
+from time import sleep
+import random
 names= ["Jhon Doe", "Jane Smith", "Alice Johnson", "Bob Brown", "Charlie Davis"]
 def main():
     for player in names:
         Player(name=player)
     print(Player.player_arrangement) # check if arrangement is done
+
+    for _ in range(1,6):
+        print(f"--- Round {_} ---")
+        for player in Player.player_arrangement:
+            dice_result = player.roll_dice()
+            print(f"{player.name} rolled and got -> {dice_result.value}")
+            sleep(random.uniform(1.5, 3.5))  # Simulate time delay between rolls
+
+        
+
 
 
 
