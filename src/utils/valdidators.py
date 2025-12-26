927c0eaac9a3d73a906e12b42fb7be2140b3bdfb
Paurakh Raj Pandey <124509859+Paurakh977@users.noreply.github.com>
2025-12-26 22:40:40 +0545

    custom valdidatros

--- src/utils/valdidators.py
+++ src/utils/valdidators.py
@@ -5,5 +5,11 @@ This module provides validation functions for various data types and formats use
 Such as validating the actions of players, game states, and input data.
 """
 
-class MaxPlayersValidator(Exception):
-    ...
+
+class MaxPlayersValidator(Exception): ...
+
+
+class InvalidPlayerActionValidator(Exception): ...
+
+
+class GameStateValidator(Exception): ...
